11f99d553edfb58a2f9fce485de34dbcb586fee5
Marvling <33942399+Marvling@users.noreply.github.com>
2020-09-11 20:43:49 -0700

    setting up

--- three_v01.js
+++ three_v01.js
@@ -1,30 +1,73 @@
 import * as THREE from './Modules/three.module.js';
 import { OrbitControls } from './Modules/OrbitControls.js';
 
-function main() {
-    const canvas = document.querySelector('#main');
-    const renderer = new THREE.WebGLRenderer({ canvas });
 
-    const fov = 75;
-    const aspect = 2;  // the canvas default
-    const near = 0.1;
-    const far = 5;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
+//INIT
+const canvas = document.querySelector('#main');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setClearColor("#000000");
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xE5E5E5)
 
-    const scene = new THREE.Scene();
+//CAMERA
+const fov = 75;
+const aspect = window.innerWidth / window.innerHeight;
+const near = 0.1;
+const far = 100;
 
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 5;
 
-    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });  // greenish blue
+//RESPONSIVE FUNCTIONS
+// window.addEventListener('resize', () => {
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+//     // controls.update();  Enable when using OrbitControls
+// })
 
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    renderer.render(scene, camera);
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const pixelRatio = window.devicePixelRatio;
+    const width = canvas.clientWidth * pixelRatio | 0;
+    const height = canvas.clientHeight * pixelRatio | 0;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
 }
 
-main();
+//LIGHTS
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(-1, 2, 4);
+
+scene.add(light);
+
+//SCENE
+const geometry = new THREE.BoxGeometry(1.99, 0.1, 4.97);
+const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });  // greenish blue
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+renderer.render(scene, camera);
+
+//RENDERING
+function render(time) {
+    time *= 0.0005;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+}
+requestAnimationFrame(render);

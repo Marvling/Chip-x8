7609a5f623ba4a009a1d3b8d6a45e5de667e348d
Marvling <33942399+Marvling@users.noreply.github.com>
2020-09-16 16:43:25 -0700

    textures ✓

--- three_v01.js
+++ three_v01.js
@@ -8,7 +8,7 @@ const canvas = document.querySelector('#main');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setClearColor("#000000");
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xE5E5E5)
+scene.background = new THREE.Color(0xE5E5E5);
 
 //CAMERA
 const fov = 75;
@@ -19,13 +19,9 @@ const far = 100;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 5;
 
-//RESPONSIVE FUNCTIONS
-// window.addEventListener('resize', () => {
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-//     camera.aspect = window.innerWidth / window.innerHeight;
-//     camera.updateProjectionMatrix();
-//     // controls.update();  Enable when using OrbitControls
-// })
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+controls.update;
 
 function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -42,29 +38,46 @@ function resizeRendererToDisplaySize(renderer) {
 }
 
 //LIGHTS
-const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-light.position.set(-1, 2, 4);
+const lightPoint = new THREE.DirectionalLight(0xFFFFFF, 1);
+lightPoint.position.set(0, 5, 2);
 
-scene.add(light);
+const lightAmbient = new THREE.AmbientLight(0x404040, 3);
+
+scene.add(lightAmbient);
+scene.add(lightPoint);
 
 //SCENE
 const loaderTex = new THREE.TextureLoader();
 const loaderMesh = new GLTFLoader();
 const loaderJson = new THREE.ObjectLoader();
 
-const geometry = new THREE.BoxGeometry(1.99, 0.1, 4.97);
+const geoChip = new THREE.BoxGeometry(1.99, 0.1, 4.97);
 
 const material = new THREE.MeshPhongMaterial({ color: 0xF1FA98 });
+const matChipBump = new THREE.MeshStandardMaterial({
+    color: 0xF1FA98,
+    bumpMap: loaderTex.load('./Assets/Chip_v2.jpg'),
+    roughness: 0.8,
+    metalness: 0.2,
+});
 const matChip = new THREE.MeshStandardMaterial({
     color: 0xF1FA98,
     roughness: 0.8,
     metalness: 0.2,
 })
+const matFaces = [
+    matChip,
+    matChip,
+    matChipBump,
+    matChip,
+    matChip,
+    matChip
+]
 
-const cube = new THREE.Mesh(geometry, matChip);
+const cube = new THREE.Mesh(geoChip, matFaces);
+cube.rotation.x = Math.PI * 0.5;
 
 scene.add(cube);
-renderer.render(scene, camera);
 
 //HELPERS
 const helperAxes = new THREE.AxesHelper();
@@ -85,8 +98,8 @@ function render(time) {
         camera.updateProjectionMatrix();
     }
 
-    cube.rotation.x = time;
-    cube.rotation.y = time;
+    // cube.rotation.x = time;
+    // cube.rotation.y = time;
 
 
     renderer.render(scene, camera);
